7962573d99ad6334cb50adae8442ab50075e669b
Yan Nunes <yan@campai.com>
2019-02-13 18:27:49 +0100

    hopefully hehe

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: npm run build
 web: bin/start-nginx ./start.sh
---
+++ buildConfig.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+let defaultConfig = JSON.stringify(require('./raw-default-config'));
+
+const configs = [
+    {
+        placeholder: '$MM_SQLSETTINGS_DATASOURCE',
+        value: process.env.MM_SQLSETTINGS_DATASOURCE,
+    },
+    {
+        placeholder: '$ELASTIC_URL',
+        value: process.env.ELASTIC_URL,
+    },
+    {
+        placeholder: '$SMTPServer',
+        value: process.env.SMTPServer,
+    },
+];
+
+for (let { placeholder, value } of configs) {
+    defaultConfig = defaultConfig.replace(placeholder, value || '');
+}
+
+fs.writeFileSync(
+    __dirname + '/default-config.json',
+    JSON.stringify(JSON.parse(defaultConfig), null, 4),
+);

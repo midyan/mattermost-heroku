48d8ca4356c14e30698068d1c412cce8b4982f0f
Yan Nunes <yan@campai.com>
2019-02-13 20:32:16 +0100

    ok

--- start.sh
+++ start.sh
@@ -14,6 +14,8 @@ trap _term SIGTERM
 ####################
 # Start Mattermost #
 ####################
+
+echo $(cat /app/default-config.json)
 bin/platform --config="/app/default-config.json" &
 
 PID=$!
